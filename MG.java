c3a52417a9cd0fc2695536d89e7e85c16e555777
Maria Galecka <mariagalecka@mbp-pereka.home>
2024-03-02 21:44:17 +0100

    MG Commit

---
+++ MG.java
@@ -0,0 +1,22 @@
+class MG {
+    public static void main (String[] args) {
+        String x = "Hello, World!";
+        String y;
+
+        int integerValue = 5;
+
+        final String z = "This text cannot be changed";
+
+        y = "Lorem ipsum dolor sit amet.";
+        y = "Reset the previous value.";
+
+        // Z = "Can we change it?";
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(integerValue);
+
+
+}
+}
